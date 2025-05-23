f3dca36866fe3e05f7b075a6073732ab21a1c90d
Michael Whit <michael@michaelwhit.com>
2025-05-23 11:38:08 -0500

    Remove old bundler in favor of nodejs bundler.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+﻿const gulp = require('gulp');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const cleanCSS = require('gulp-clean-css');
+
+// Bundle and minify JavaScript
+gulp.task('js', () => {
+    return gulp.src(['wwwroot/js/site.js', 'wwwroot/js/clock-animation.js'])
+        .pipe(concat('site.min.js'))
+        .pipe(terser({
+            mangle: { toplevel: true },
+            compress: true
+        }))
+        .pipe(gulp.dest('wwwroot/js'));
+});
+
+// Bundle and minify CSS
+gulp.task('css', () => {
+    return gulp.src(['wwwroot/css/site.css', 'wwwroot/css/clock-animation.css', 'wwwroot/css/layout.css'])
+        .pipe(concat('site.min.css'))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+// Default task: Run both js and css tasks
+gulp.task('default', gulp.parallel('js', 'css'));
+
+// Watch task for development
+gulp.task('watch', () => {
+    gulp.watch([
+        'wwwroot/js/site.js',
+        'wwwroot/js/clock-animation.js',
+        'wwwroot/css/site.css',
+        'wwwroot/css/clock-animation.css',
+        'wwwroot/css/layout.css'
+    ], { ignoreInitial: false }, gulp.series('default'));
+});
