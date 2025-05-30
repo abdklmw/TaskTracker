788705541445dca9ed8fe82e1723156beb003747
Michael Whit <michael@michaelwhit.com>
2025-05-30 14:36:34 -0500

    Fix settings update so all fields are successfully saved,

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -56,6 +56,12 @@ namespace TaskTracker.Controllers
                     existingSettings.PaymentInformation = settings.PaymentInformation;
                     existingSettings.ThankYouMessage = settings.ThankYouMessage;
                     existingSettings.DefaultHourlyRate = settings.DefaultHourlyRate;
+                    existingSettings.SmtpServer = settings.SmtpServer;
+                    existingSettings.SmtpPort = settings.SmtpPort;
+                    existingSettings.SmtpSenderEmail = settings.SmtpSenderEmail;
+                    existingSettings.SmtpUsername = settings.SmtpUsername;
+                    existingSettings.SmtpPassword = settings.SmtpPassword;
+                    existingSettings.InvoiceTemplate = settings.InvoiceTemplate;
                     _context.Update(existingSettings);
                 }
                 await _context.SaveChangesAsync();
