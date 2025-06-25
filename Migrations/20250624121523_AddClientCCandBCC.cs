f9185fbfc16b0ff91774e97f1ecf2d4bbd9a6b6a
Michael Whit <michael@michaelwhit.com>
2025-06-25 13:56:18 -0500

    Add migration.

---
+++ Migrations/20250624121523_AddClientCCandBCC.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientCCandBCC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BCC",
+                table: "Clients",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CC",
+                table: "Clients",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BCC",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "CC",
+                table: "Clients");
+        }
+    }
+}
